fea2415bcbf0f20176e99252d42f44bb47046c0a
wody34 <k2820997@gmail.com>
2017-09-03 20:16:29 +0900

    Update

--- LabSolutions/Lab1/lab1_1a.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-int main(int argc, char* argv[]) {
-	int x = 3;
-	int err = execl(argv[1], argv[1], argv[2], NULL);	//WARN: Actually, second argument should not include the path.
-	printf("error = %d, x = %d\n", err, x);
-}
